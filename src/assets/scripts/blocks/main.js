166615d0054c7b20a33109d891a0bf7d304a2581
Soveliy <nikita.elagin2012@yandex.ru>
2023-03-27 04:32:28 +0300

    add gulp

---
+++ src/assets/scripts/blocks/main.js
@@ -0,0 +1,15 @@
+
+
+
+
+$(document).ready(function() {
+  
+	$('#pagepiling').pagepiling({
+	   
+	});
+    const heroSlider = new Swiper('.hero__slider', {
+        speed: 400,
+        spaceBetween: 0,
+        slidesPerView: 1
+    });
+});
---
+++ src/assets/scripts/script.js
@@ -0,0 +1,13 @@
+// script.js
+
+// Script
+
+// // Plugins
+@@include('./plugins/jquery-3.6.4.min.js');
+@@include('./plugins/lazysizes.min.js');
+@@include('./plugins/jquery.pagepiling.min.js');
+@@include('./plugins/swiper-bundle.min.js');
+// // Blocks
+document.addEventListener('DOMContentLoaded', () => {
+   @@include("./blocks/main.js")
+});
